b6340570082bd7191356ffa82576a98ca3744e19
soobin090630 <soobin090630@gmail.com>
2026-08-21 18:55:43 +0900

    Connect Firebase web app

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,11 @@
 // Firebase Console → Project settings → Your apps → Web app의 값을 넣으세요.
 // 이 파일의 config는 비밀번호가 아니며, Firebase Auth 도메인 제한과 Rules가 보안을 담당합니다.
 export const firebaseConfig = {
-  apiKey: 'PASTE_YOUR_FIREBASE_API_KEY',
-  authDomain: 'PASTE_YOUR_PROJECT.firebaseapp.com',
-  projectId: 'PASTE_YOUR_PROJECT_ID',
-  storageBucket: 'PASTE_YOUR_PROJECT.appspot.com',
-  messagingSenderId: 'PASTE_YOUR_SENDER_ID',
-  appId: 'PASTE_YOUR_APP_ID'
+  apiKey: 'AIzaSyDvCrm9kGb9eUk_u3aLUkKdPIA_8M7Ol3w',
+  authDomain: 'moare-auth.firebaseapp.com',
+  projectId: 'moare-auth',
+  storageBucket: 'moare-auth.firebasestorage.app',
+  messagingSenderId: '696840303220',
+  appId: '1:696840303220:web:4b7c881417e3e0338b5721',
+  measurementId: 'G-5FFDHDZJZ5'
 };
